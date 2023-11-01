36d16ec488b171069dae3eccf0958baeb655cbf1
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-11-01 15:01:03 -0500

    Created a table using JavaScript.

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,29 +1,31 @@
-window.addEventListener("load", () => {
-  document.body.style.background = "cyan";
-});
+// window.addEventListener("load", () => {
+//   document.body.style.background = "white";
+// });
 
 const header = document.getElementById("header").innerHTML = "FitPro";
 const headerDiv = document.getElementById("headerDiv");
 headerDiv.classList.add("text-center");
 
-const paragraph = document.getElementById("paragraph").innerHTML = "Enter the muscle group you would like exercises for.";
+// const paragraph = document.getElementById("paragraph").innerHTML = "Enter the muscle group you would like exercises for.";
 
-function getExercises(){
-  const person = {
-    name: "John",
-    age: 30,
-    city: "New York"
-};
+// // getExercises creates person object
+// function getExercises(){
+//   const person = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// };
 
-document.getElementById("demo").innerHTML = person.name;
+// // this line displays the persons name
+// document.getElementById("demo").innerHTML = person.name;
+// }
 
-
-const centerText = () => {    
-  var centerText = document.createElement('p');
-  centerText.innerText = 'Center Text';
-  centerText.style.color = 'black';
-  centerText.style.textAlign = 'center';
-  document.body.appendChild(centerText);
-} 	 
-centerText();
-}
+// // this shows an example of a centered text
+// const centerText = () => {    
+//   var centerText = document.createElement('p');
+//   centerText.innerText = 'Center Text';
+//   centerText.style.color = 'black';
+//   centerText.style.textAlign = 'center';
+//   document.body.appendChild(centerText);
+// } 	 
+// centerText();
